7aebee8fed14df291c5a61e4e47ef73fcef6835d
Attila Dröszler <attila.droszler@emarsys.com>
2020-01-22 11:13:51 +0100

    remove unused constants from test helper
    Co-authored-by: gergely.fehervari@emarsys.com

--- test/Helpers/Constants.php
+++ test/Helpers/Constants.php
@@ -5,8 +5,4 @@ namespace Emartech\Chunkulator\Test\Helpers;
 class Constants
 {
     const TRIGGER_ID = 'triggerId';
-    const CUSTOMER_ID = 1;
-    const PROGRAM_ID = 12;
-    const SOURCE_LIST_ID = 123;
-    const TARGET_LIST_ID = 1234;
 }
